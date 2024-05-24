c2ce911ac7e6e61072c497ef329c9fb02edd0a07
WangariNelly <nellywangari30@gmail.com>
2024-05-24 15:00:45 +0300

    Slider functionality for the previous and next buttons

--- app.js
+++ app.js
@@ -6,32 +6,35 @@ slides.forEach((slide,index) => {
     slide.style.left = `${index * 100}`;
 });
 
-let countStart = 0;
+let currentSlide = 0;
 
 nextButton.addEventListener('click', () => {
-    countStart++
+    currentSlide++
     displayPage();
 });
 
 prevButton.addEventListener('click', () => {
-    countStart--;
+    currentSlide--;
     displayPage();
 })
-if (countStart < slides.length - 1){
-    nextButton.style.display = "block";
-} else {
-    nextButton.style.display = "none"
-}
 
-if (countStart > slides.length - 1){
-    prevButton.style.display = "block";
-} else {
-    prevButton.style.display = "none"
-}
 
 function displayPage(){
+    // nextButton.style.display = currentSlide === slides.length - 1 ? 'none' : 'block';
+    // prevButton.style.display = currentSlide > 0 ? 'none' : 'block';
+    if (currentSlide < slides.length - 1){
+        nextButton.style.display = "block";
+    } else {
+        nextButton.style.display = "none"
+    }
+    
+    if (currentSlide  >  0){
+        prevButton.style.display = "block";
+    } else {
+        prevButton.style.display = "none"
+    }
     slides.forEach((slide) => {
-        slide.style.transform = `translateX(-${counter * 100}%)`;
+        slide.style.transform = `translateX(-${currentSlide * 100}%)`;
     })
 }
 
